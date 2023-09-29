a89520b2ba693888c50b354056edda3b824d1562
Mazoda <oabumutlak@gmail.com>
2023-09-29 10:22:01 +0300

    Removed: comments

--- src/Services/Firebase.ts
+++ src/Services/Firebase.ts
@@ -1,12 +1,7 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 
 import { getFirestore, collection, getDocs } from "firebase/firestore/lite";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyDwMs_Y1a5WR7AuLOnTsqpHo4meQWBgw50",
   authDomain: "cleanstead-fe22f.firebaseapp.com",
@@ -17,7 +12,7 @@ const firebaseConfig = {
   measurementId: "G-J76KJX973S",
 };
 
-// Initialize Firebase
+
 const app = initializeApp(firebaseConfig);
 
 const db = getFirestore(app);
