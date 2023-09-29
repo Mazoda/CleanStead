f5665e18b7da1fa275d436e6a4da4a3c9f32a5bd
Mazoda <oabumutlak@gmail.com>
2023-09-29 10:20:51 +0300

    Configure: Vite Css modules in production

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+type ViteConfigInput = {
+  mode: string;
+  command: string;
+};
+export default (args: ViteConfigInput) => {
+  const generateScopedName =
+    args.mode === "production" ? "[hash:base64:2]" : "[local]_[hash:base64:2]";
+
+  return defineConfig({
+    plugins: [react()],
+    css: {
+      modules: {
+        localsConvention: "camelCase",
+        generateScopedName,
+      },
+    },
+  });
+};
