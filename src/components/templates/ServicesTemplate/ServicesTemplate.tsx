3e06cc808aae465d0a962b8e3feb216b835d7e1b
Mazoda <oabumutlak@gmail.com>
2023-10-03 03:59:29 +0300

    Edit: changed style

--- src/components/templates/ServicesTemplate/ServicesTemplate.tsx
+++ src/components/templates/ServicesTemplate/ServicesTemplate.tsx
@@ -1,37 +1,33 @@
-import { Box, Grid } from "@mui/material";
+import { Grid } from "@mui/material";
 import { ServiceType } from "../../../types";
 import { ServiceCard } from "../../organisms/ServiceCard";
 import { WrappedHeading } from "../../atoms/WrappedHeading";
 
 export function ServicesTemplate({ services }: { services: ServiceType[] }) {
   return (
-    <>
-      <Box
+    <div>
+      <WrappedHeading heading="الخدمات التي نقدمها" />
+      <Grid
+        container
+        columns={{ xs: 6, sm: 6, md: 12 }}
+        spacing={{ xs: 4 }}
         sx={{
-          marginBlock: "3rem",
-          display: "flex",
-          flexDirection: "column",
-          gap: "2.75rem",
-          alignItems: "center",
-          justifyContent: "center",
+          paddingBlock: "1rem",
         }}
       >
-        <WrappedHeading heading="الخدمات التي نقدمها" />
-        <Grid container columns={{ xs: 6, sm: 6, md: 12 }} spacing={{ xs: 4 }}>
-          {services
-            ? services.map((service: ServiceType) => (
-                <Grid item xs={6} sm={3} md={4} key={service.serviceID}>
-                  <ServiceCard
-                    serviceID={service.serviceID}
-                    imageUrl={service.imageUrl}
-                    title={service.title}
-                    description={service.description}
-                  ></ServiceCard>
-                </Grid>
-              ))
-            : "no data found"}
-        </Grid>
-      </Box>
-    </>
+        {services
+          ? services.map((service: ServiceType) => (
+              <Grid item xs={6} sm={3} md={4} key={service.serviceID}>
+                <ServiceCard
+                  serviceID={service.serviceID}
+                  imageUrl={service.imageUrl}
+                  title={service.title}
+                  description={service.description}
+                ></ServiceCard>
+              </Grid>
+            ))
+          : "no data found"}
+      </Grid>
+    </div>
   );
 }
